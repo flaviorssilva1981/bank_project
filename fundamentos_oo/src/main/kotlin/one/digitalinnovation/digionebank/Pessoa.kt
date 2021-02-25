18ca22d36918994252ddaa01f1cd5a27f9b41842
Flavio <flavio.rssilva@gmail.com>
2021-02-25 19:39:53 -0300

    private modifier in the "cpf"

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,12 +1,18 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+//creating classes "Pessoa"
     var nome: String = "Flavio"
+
     var cpf: String = "213.654.658-97"
+    private set
+//"private" modified of the variable
 
 }
+
 fun main() {
     val flavio = Pessoa()
+//instantiating the class "Pessoa"
     println(flavio.nome)
     println(flavio.cpf)
     }
